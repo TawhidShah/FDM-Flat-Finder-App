f287b9625fabbb2ea8f784664cd4453d28fc7c13
Tawhid Shah <tawhidshahdev@gmail.com>
2024-03-18 17:08:05 +0000

    Corrected overflow

--- app/layout.jsx
+++ app/layout.jsx
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
         )}
       >
         <Header />
-        <main className="flex h-full flex-1 flex-col overflow-scroll">
+        <main className="flex h-full flex-1 flex-col overflow-auto">
           {children}
         </main>
       </body>
