dd906fd38a8f533ec87bc98cd3375f6afc5cbff3
Faizan Aftab <120399968+Faizan477@users.noreply.github.com>
2024-03-21 10:38:23 +0000

    Basic start to listings page
    Installed axios, made a start with the listing page

--- app/listings/page.jsx
+++ app/listings/page.jsx
@@ -1,59 +1,50 @@
 "use client";
 import React, { useState } from 'react'
-import './listingPage.css'
-const Listings = () => 
-{
-  const [searchLocation,setSearchLocation]=useState("");
-  const changeLocationState=(e)=>
-  {
+const Listings = () => {
+  const [searchLocation, setSearchLocation] = useState("");
+  const changeLocationState = (e) => {
     setSearchLocation(e.target.value);
   }
-  const checkForEmptyInput=(searchLocation)=>
-  {
-    if(searchLocation=="")
-    {
+  const checkForEmptyInput = (searchLocation) => {
+    if (searchLocation == "") {
       console.log("Please enter a location in which to search for properties.");
     }
-    else
-    {
-      fetchLocation({searchLocation});
+    else {
+      fetchLocation({ searchLocation });
     }
-    
+
   }
-  const fetchLocation=async(inputLocation)=>
-  {
-    const headers=
+  const fetchLocation = async (inputLocation) => {
+    const headers =
     {
       'X-RapidAPI-Key': process.e.RAPID_API_KEY,
       'X-RapidAPI-Host': 'zoopla.p.rapidapi.com'
     }
-    const parameters=
+    const parameters =
     {
-      locationPrefix:`${inputLocation}`
+      locationPrefix: `${inputLocation}`
     }
-    const response=await axios.get('https://zoopla.p.rapidapi.com/v2/auto-complete',{headers,parameters});
+    const response = await axios.get('https://zoopla.p.rapidapi.com/v2/auto-complete', { headers, parameters });
     //here, check if the response contains any data. If it doesn't display a message indicating what the issue may be.
     fetchPropertyData(inputLocation)
   }
-  const fetchPropertyData=(location)=>
-  {
-    const headers=
+  const fetchPropertyData = (location) => {
+    const headers =
     {
       'X-RapidAPI-Key': process.e.RAPID_API_KEY,
       'X-RapidAPI-Host': 'zoopla.p.rapidapi.com'
     }
-    const parameters=
+    const parameters =
     {
-      
+
     }
   }
-  const submitButton=document.getElementById("submitButton");
   return (
     <div id="mainContainer">
       <p>Search for your dream property</p>
       <form>
-          <input id="inputSearchLocation" type="text" placeholder="Enter a city/place name" value={searchLocation} onChange={changeLocationState} required></input>
-          <button id="submitButton" onClick={checkForEmptyInput(location)}>Search</button>
+        <input id="inputSearchLocation" type="text" placeholder="Enter a city/place name" value={searchLocation} onChange={changeLocationState} required></input>
+        <button id="submitButton">Search</button>
       </form>
     </div>
   )
