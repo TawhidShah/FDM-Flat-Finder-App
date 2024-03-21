86fc4cf9c6472b4aadcaea57c8b119fe2671ff3c
Faizan Aftab <120399968+Faizan477@users.noreply.github.com>
2024-03-21 09:42:57 +0000

    Update page.jsx for listings
    Basic start for main listings page

--- app/listings/page.jsx
+++ app/listings/page.jsx
@@ -1,8 +1,61 @@
-import React from 'react'
-
-const Listings = () => {
+"use client";
+import React, { useState } from 'react'
+import './listingPage.css'
+const Listings = () => 
+{
+  const [searchLocation,setSearchLocation]=useState("");
+  const changeLocationState=(e)=>
+  {
+    setSearchLocation(e.target.value);
+  }
+  const checkForEmptyInput=(searchLocation)=>
+  {
+    if(searchLocation=="")
+    {
+      console.log("Please enter a location in which to search for properties.");
+    }
+    else
+    {
+      fetchLocation({searchLocation});
+    }
+    
+  }
+  const fetchLocation=async(inputLocation)=>
+  {
+    const headers=
+    {
+      'X-RapidAPI-Key': process.e.RAPID_API_KEY,
+      'X-RapidAPI-Host': 'zoopla.p.rapidapi.com'
+    }
+    const parameters=
+    {
+      locationPrefix:`${inputLocation}`
+    }
+    const response=await axios.get('https://zoopla.p.rapidapi.com/v2/auto-complete',{headers,parameters});
+    //here, check if the response contains any data. If it doesn't display a message indicating what the issue may be.
+    fetchPropertyData(inputLocation)
+  }
+  const fetchPropertyData=(location)=>
+  {
+    const headers=
+    {
+      'X-RapidAPI-Key': process.e.RAPID_API_KEY,
+      'X-RapidAPI-Host': 'zoopla.p.rapidapi.com'
+    }
+    const parameters=
+    {
+      
+    }
+  }
+  const submitButton=document.getElementById("submitButton");
   return (
-    <div>LISTINGS</div>
+    <div id="mainContainer">
+      <p>Search for your dream property</p>
+      <form>
+          <input id="inputSearchLocation" type="text" placeholder="Enter a city/place name" value={searchLocation} onChange={changeLocationState} required></input>
+          <button id="submitButton" onClick={checkForEmptyInput(location)}>Search</button>
+      </form>
+    </div>
   )
 }
 
