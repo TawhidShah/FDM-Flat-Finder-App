f48042af5b0060f763c1b47149c4ec42e668529e
Faizan Aftab <120399968+Faizan477@users.noreply.github.com>
2024-03-23 13:57:44 +0000

    Update page.jsx, made a start with fetching data

--- app/listings/page.jsx
+++ app/listings/page.jsx
@@ -1,53 +1,61 @@
 "use client";
+import axios from "axios"
 import React, { useState } from 'react'
 const Listings = () => {
   const [searchLocation, setSearchLocation] = useState("");
   const changeLocationState = (e) => {
     setSearchLocation(e.target.value);
   }
-  const checkForEmptyInput = (searchLocation) => {
-    if (searchLocation == "") {
-      console.log("Please enter a location in which to search for properties.");
+  const handleButtonClick = (event) => {
+    event.preventDefault();
+    const locationToSearch = searchLocation;
+    if (locationToSearch == '') {
+      //display mesage- invalid input
     }
     else {
-      fetchLocation({ searchLocation });
+      fetchLocation(locationToSearch);
     }
-
   }
   const fetchLocation = async (inputLocation) => {
     const headers =
     {
-      'X-RapidAPI-Key': process.e.RAPID_API_KEY,
-      'X-RapidAPI-Host': 'zoopla.p.rapidapi.com'
+      'X-RapidAPI-Key': `${process.env.RAPID_API_KEY}`,
+      'X-RapidAPI-Host': 'zoopla4.p.rapidapi.com'
     }
-    const parameters =
+    const params =
     {
-      locationPrefix: `${inputLocation}`
+      location: `${inputLocation}`
     }
-    const response = await axios.get('https://zoopla.p.rapidapi.com/v2/auto-complete', { headers, parameters });
-    //here, check if the response contains any data. If it doesn't display a message indicating what the issue may be.
-    fetchPropertyData(inputLocation)
+    try {
+      const response = await axios.get('https://zoopla4.p.rapidapi.com/locations', { headers, params });
+      console.log(response);
+      //here, check if the response contains any data. If it doesn't display a message indicating what the issue may be.
+      fetchPropertyData(inputLocation)
+    }
+    catch
+    {
+      console.log("");
+    }
   }
   const fetchPropertyData = (location) => {
     const headers =
     {
-      'X-RapidAPI-Key': process.e.RAPID_API_KEY,
-      'X-RapidAPI-Host': 'zoopla.p.rapidapi.com'
+      'X-RapidAPI-Key': `${process.env.RAPID_API_KEY}`,
+      'X-RapidAPI-Host': 'zoopla4.p.rapidapi.com'
     }
     const parameters =
     {
-
+      //parameters for the search go here 
     }
   }
   return (
     <div id="mainContainer">
       <p>Search for your dream property</p>
       <form>
-        <input id="inputSearchLocation" type="text" placeholder="Enter a city/place name" value={searchLocation} onChange={changeLocationState} required></input>
-        <button id="submitButton">Search</button>
+        <input id="inputSearchLocation" type="text" placeholder="Enter a city/place name" value={searchLocation} onChange={changeLocationState}></input>
+        <button id="submitButton" onClick={handleButtonClick}>Search</button>
       </form>
     </div>
   )
 }
-
 export default Listings
