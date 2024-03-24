c51adbd1a3e33aa3285fb48728d79a667c397a0f
Faizan Aftab <120399968+Faizan477@users.noreply.github.com>
2024-03-24 19:52:15 +0000

    Update listings page

--- app/listings/page.jsx
+++ app/listings/page.jsx
@@ -1,8 +1,11 @@
 "use client";
-import axios from "axios"
-import React, { useState } from 'react'
+import axios from "axios";
+import React, { useState } from 'react';
+import "./listings.css";
+import propertyListing from "./propertyListing";
 const Listings = () => {
   const [searchLocation, setSearchLocation] = useState("");
+  const [displayEmptyInputWarning, setDisplayEmptyInputWarning] = useState(false);
   const changeLocationState = (e) => {
     setSearchLocation(e.target.value);
   }
@@ -10,50 +13,61 @@ const Listings = () => {
     event.preventDefault();
     const locationToSearch = searchLocation;
     if (locationToSearch == '') {
-      //display mesage- invalid input
+      setDisplayEmptyInputWarning(true);
     }
     else {
+      setDisplayEmptyInputWarning(false);
       fetchLocation(locationToSearch);
     }
   }
+
   const fetchLocation = async (inputLocation) => {
     const headers =
     {
       'X-RapidAPI-Key': '',
-      'X-RapidAPI-Host': 'zoopla4.p.rapidapi.com'
+      'X-RapidAPI-Host': 'rightmove4.p.rapidapi.com'
     }
     const params =
     {
       location: `${inputLocation}`
     }
     try {
-      const response = await axios.get('https://zoopla4.p.rapidapi.com/locations', { headers, params });
-      console.log(response);
-      //here, check if the response contains any data. If it doesn't display a message indicating what the issue may be.
-      fetchPropertyData(inputLocation)
+      const response = await axios.get('https://rightmove4.p.rapidapi.com/locations', { headers, params });
+      console.log(`We are now fetching data with the location ${response.data.data[0].key}`);
+      fetchPropertyData(response.data.data[0].key); //will be changed to provide a dropdown enabling the user to select a more specific location
     }
     catch
     {
-      console.log("");
+      console.log("An error has occured while fetching the location that you input.");
     }
   }
-  const fetchPropertyData = (location) => {
+
+  const fetchPropertyData = async (location) => {
     const headers =
     {
       'X-RapidAPI-Key': '',
-      'X-RapidAPI-Host': 'zoopla4.p.rapidapi.com'
+      'X-RapidAPI-Host': 'rightmove4.p.rapidapi.com'
     }
-    const parameters =
+    const params =
     {
-      //parameters for the search go here 
+      locationKey: `${location}`
+    }
+    try {
+      const response = await axios.get('https://rightmove4.p.rapidapi.com/properties/rent', { headers, params });
+      console.log(response.data);
+    }
+    catch
+    {
+      console.log("An error has occured while fetching property results. Please try again.");
     }
   }
   return (
     <div id="mainContainer">
-      <p>Search for your dream property</p>
+      <h1 id="infoText">Search for your dream property</h1>
       <form>
-        <input id="inputSearchLocation" type="text" placeholder="Enter a city/place name" value={searchLocation} onChange={changeLocationState}></input>
+        <input className="inputSearchLocation" type="text" placeholder="Enter a city/place name" value={searchLocation} onChange={changeLocationState}></input>
         <button id="submitButton" onClick={handleButtonClick}>Search</button>
+        {displayEmptyInputWarning == true ? <div id="emptyInputWarning"><p>Please ensure that you fill in the box above.</p> </div> : <p></p>}
       </form>
     </div>
   )
