914989af7182a1a303b04f9bdef79f83b711bd7c
Yichen727 <128410846+Yichen727@users.noreply.github.com>
2024-03-24 19:17:48 +0000

    update listing

--- app/listings/[id]/page.jsx
+++ app/listings/[id]/page.jsx
@@ -1,34 +1,62 @@
 import React from 'react';
-import mockListings from '.mockListings';
+import mockListings from './mockListings';
+import './page.css';
+
 
 const Listing = ({ params }) => {
-  const listingId = parseInt(params.id); // Assuming params.id is a string, convert it to an integer
+  const { id } = params;
+  const listing = mockListings.find(listing => listing.id.toString() === id);
 
-  // Find the listing with the matching ID
-  const listing = mockListings.find(listing => listing.id === listingId);
+  
+  
 
   if (!listing) {
     return <div>Listing not found</div>;
   }
 
   return (
-    <div>
-      <h2>{listing.title}</h2>
-      <p>{listing.description}</p>
-      <p>Price: {listing.price}</p>
-      <p>Property Type: {listing.propertyType}</p>
-      <p>Owner Type: {listing.ownerType}</p>
-      <p>Address: {listing.address}</p>
-      <p>Nearby Stations: {listing.nearbyStations.join(', ')}</p>
-      <p>Bedrooms: {listing.bedrooms}</p>
-      <p>Bathrooms: {listing.bathrooms}</p>
-      <p>Area: {listing.area}</p>
-      <div>
-        {listing.images.map((image, index) => (
-          <img key={index} src={image} alt={`Image ${index}`} />
-        ))}
+    <div className='container'>
+      <div className="images">
+        <img src={listing.images[0]} alt="First Image" />
       </div>
-      <a href={listing.IMAGES_FROM} target="_blank" rel="noopener noreferrer">View More Details</a>
+
+      <form className="booking_form" method="GET" action="">
+        <fieldset>
+          <legend id="formtitle">BOOKING FORM</legend>
+          <p id="formInfo">
+            <label for="name">Name:</label><br />
+            <input type="text" id="name" name="name" required /><br/>
+            <label for="email">Email:</label><br/>
+            <input type="email" id="email" name="email" required /><br/>
+          </p>
+        <button id="formButton" type="submit">Submit</button>
+        </fieldset>
+  
+      </form>
+
+      <div className="owner">
+        <a href={listing.IMAGES_FROM} target="_blank" rel="noopener noreferrer">Owner</a>
+      </div>
+
+      <div className="chatButton">
+        <button>Chat with agent</button>
+      </div>
+    
+      <div className="details">
+        <h2>{listing.title}</h2>
+        <ul>
+          <li>Description: {listing.description}</li>
+          <li>Price: ${listing.price}</li>
+          <li>Property Type: {listing.propertyType}</li>
+          <li>Number of Bedrooms: {listing.bedrooms}</li>
+          <li>Number of Bathrooms: {listing.bathrooms}</li>
+          <li>Area: {listing.area}</li>
+          <li>City: {listing.city}</li>
+          <li>Address: {listing.address}</li>
+          <li>Nearby Stations: {listing.nearbyStations.join(', ')}</li>
+        </ul>
+      </div>
+        
     </div>
   );
 }
