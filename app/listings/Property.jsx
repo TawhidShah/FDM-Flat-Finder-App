bed0d078cb77722faa4532e51389376ca4a16dbc
Faizan Aftab <120399968+Faizan477@users.noreply.github.com>
2024-03-26 12:15:35 +0000

    Update page.jsx, Add poperty.jsx
    Made a start with rendering property data

---
+++ app/listings/Property.jsx
@@ -0,0 +1,6 @@
+function Property(props) {
+    return (<>
+        <p>{props.description}</p>
+    </>)
+}
+export default Property
--- app/listings/propertyListing.jsx
+++
@@ -1,7 +0,0 @@
-function propertyListing(props) {
-    return
-    (
-        <div></div>
-    )
-}
-export default propertyListing;
