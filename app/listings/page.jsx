1c70efb2c28e4b80f18a305f882924b413e055aa
Faizan Aftab <120399968+Faizan477@users.noreply.github.com>
2024-03-26 12:15:35 +0000

    Update page.jsx, Add poperty.jsx
    Made a start with rendering property data

--- app/listings/page.jsx
+++ app/listings/page.jsx
@@ -1,11 +1,13 @@
 "use client";
 import axios from "axios";
 import React, { useState } from 'react';
-import "./listings.css";
-import propertyListing from "./propertyListing";
+import './listings.css';
+import Property from './Property.jsx';
 const Listings = () => {
   const [searchLocation, setSearchLocation] = useState("");
   const [displayEmptyInputWarning, setDisplayEmptyInputWarning] = useState(false);
+  const [properties, setProperties] = useState([]);
+
   const changeLocationState = (e) => {
     setSearchLocation(e.target.value);
   }
@@ -34,7 +36,7 @@ const Listings = () => {
     try {
       const response = await axios.get('https://rightmove4.p.rapidapi.com/locations', { headers, params });
       console.log(`We are now fetching data with the location ${response.data.data[0].key}`);
-      fetchPropertyData(response.data.data[0].key); //will be changed to provide a dropdown enabling the user to select a more specific location
+      fetchProperties(response.data.data[0].key); //could be changed to provide a dropdown enabling the user to select a more specific location
     }
     catch
     {
@@ -42,33 +44,42 @@ const Listings = () => {
     }
   }
 
-  const fetchPropertyData = async (location) => {
+  const fetchProperties = async (location) => {
     const headers =
     {
       'X-RapidAPI-Key': '',
-      'X-RapidAPI-Host': 'rightmove4.p.rapidapi.com'
+      'X-RapidAPI-Host': 'uk-real-estate-rightmove.p.rapidapi.com'
     }
     const params =
     {
-      locationKey: `${location}`
+      identifier: `${location}`,
+      property_type: 'Flat'
     }
     try {
-      const response = await axios.get('https://rightmove4.p.rapidapi.com/properties/rent', { headers, params });
-      console.log(response.data);
+      const response = await axios.get('https://uk-real-estate-rightmove.p.rapidapi.com/rent/property-to-rent', { headers, params });
+      setProperties(response.data.data);
     }
     catch
     {
       console.log("An error has occured while fetching property results. Please try again.");
     }
   }
+
   return (
     <div id="mainContainer">
-      <h1 id="infoText">Search for your dream property</h1>
+      <h1 id="infoText">Search for your dream property to rent</h1>
       <form>
         <input className="inputSearchLocation" type="text" placeholder="Enter a city/place name" value={searchLocation} onChange={changeLocationState}></input>
         <button id="submitButton" onClick={handleButtonClick}>Search</button>
         {displayEmptyInputWarning == true ? <div id="emptyInputWarning"><p>Please ensure that you fill in the box above.</p> </div> : <p></p>}
       </form>
+      <div id="properties">
+        {properties.map((property) => <Property key={property.id} numBath={property.bathrooms}
+          numBed={property.bedrooms} estateAgent={property.customer.branchDisplayName}
+          address={property.displayAddress} price={property.price.displayPrices[0].displayPrice}
+          image={property.propertyImages.mainImageSrc} propertyType={property.propertySubType}
+          description={property.propertyTypeFullDescription} summary={property.summary}></Property>)}
+      </div>
     </div>
   )
 }
