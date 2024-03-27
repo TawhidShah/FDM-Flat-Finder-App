cc5be235645d026ac0d61f4226735b32885099b0
Faizan Aftab <120399968+Faizan477@users.noreply.github.com>
2024-03-27 16:24:33 +0000

    Update page.jsx and Property.jsx

--- app/listings/Property.jsx
+++ app/listings/Property.jsx
@@ -1,6 +1,16 @@
 function Property(props) {
-    return (<>
-        <p>{props.description}</p>
-    </>)
+    return (<div>
+        <div id="propertyCard">
+            <div id="mainInfo">
+                <h1>{props.description}</h1>
+                <h1>{props.address}</h1>
+                <p>{props.numBath} bathrooms</p>
+            </div>
+            <div id="price">
+                <h1>{props.price}</h1>
+            </div>
+        </div>
+        <img src={props.image} alt="Property"></img>
+    </div>)
 }
 export default Property
