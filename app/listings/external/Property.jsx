d7bd2b82590155749d7ce12d1fb3f2ef85e0a1a3
Faizan Aftab <120399968+Faizan477@users.noreply.github.com>
2024-03-29 16:27:38 +0000

    Update page.jsx, included input validation

--- app/listings/external/Property.jsx
+++ app/listings/external/Property.jsx
@@ -1,16 +1,16 @@
 function Property(props) {
     return (<div>
         <div id="propertyCard">
-            <div id="mainInfo">
+            <div id="picture">
+                <img src={props.image} alt="Property"></img>
+            </div>
+            <div id="info">
                 <h1>{props.description}</h1>
                 <h1>{props.address}</h1>
                 <p>{props.numBath} bathrooms</p>
-            </div>
-            <div id="price">
                 <h1>{props.price}</h1>
             </div>
         </div>
-        <img src={props.image} alt="Property"></img>
     </div>)
 }
 export default Property
