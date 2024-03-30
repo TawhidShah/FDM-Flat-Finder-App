e5d2226f33751862485d32226feb430d314220d4
Tawhid Shah <tawhidshahdev@gmail.com>
2024-03-30 15:35:48 +0000

    Modified Listing get endpoint to populate owner field

--- app/api/listings/route.js
+++ app/api/listings/route.js
@@ -5,7 +5,7 @@ import { UserProfile } from "@/models/UserProfile";
 
 export async function GET() {
   await mongooseConnect();
-  const listings = await Listing.find();
+  const listings = await Listing.find().populate("owner");
   return NextResponse.json(listings);
 }
 
