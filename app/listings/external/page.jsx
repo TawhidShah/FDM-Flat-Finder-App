b585913b94d7e9dfc4ffda72d0de47f0512e8819
Faizan Aftab <120399968+Faizan477@users.noreply.github.com>
2024-04-03 12:52:19 +0100

    Updating page.jsx for US functionality

--- app/listings/external/page.jsx
+++ app/listings/external/page.jsx
@@ -173,6 +173,30 @@ const Listings = () => {
       );
     }
   };
+  const fetchLocationUS = async (inputLocation, minPrice, maxPrice, minBed, maxBed) => {
+    const headers =
+    {
+      'X-RapidAPI-Key': 'd3e773cfbbmsh969fb38e5d05e4cp1eb2e9jsnbf64876767cd',
+      'X-RapidAPI-Host': 'zillow-stable.p.rapidapi.com'
+    };
+    const params =
+    {
+      query: `${inputLocation}`
+    };
+    try {
+      setLoading(true);
+      const response = await axios.get(
+        "https://zillow-stable.p.rapidapi.com/api/general/autocomplete",
+        { headers, params },
+      );
+      setPropertyRequestSubmitted(true);
+    } catch {
+      setLoading(false);
+      window.alert(
+        "An error has occured while fetching the location that you input.",
+      );
+    }
+  }
   return (
     <div id="mainContainer">
       {loading == true ? (
