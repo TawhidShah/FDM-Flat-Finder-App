272ae098940e26af23681046f3e79508f4fe0671
Tawhid Shah <tawhidshahdev@gmail.com>
2024-04-03 19:13:26 +0100

    Added populate for listings back

--- app/api/users/[username]/route.js
+++ app/api/users/[username]/route.js
@@ -10,7 +10,7 @@ export async function GET(request, context) {
   if (!username) {
     return NextResponse.json({ error: "Missing username" }, { status: 400 });
   }
-  const user = await UserProfile.findOne({ username });
+  const user = await UserProfile.findOne({ username }).populate("listings");
 
   if (!user) {
     return NextResponse.json({ error: "User not found" }, { status: 404 });
