096018d9e79f354cb1ee8990f806cb1704906bd9
Tawhid Shah <tawhidshahdev@gmail.com>
2024-04-05 05:27:11 +0100

    Started color theme change

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ export default function Home() {
       <div className="mt-10">
         <Link
           href="/listings"
-          className="hover:bg-primary-dark inline-block rounded-lg bg-primary p-3 px-6 text-2xl text-primary-foreground transition-colors"
+          className="inline-block rounded-lg bg-primary p-3 px-6 text-2xl text-black transition-colors hover:bg-white"
         >
           Start Your Search
         </Link>
--- components/Header.jsx
+++ components/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
     <header className="bg-[#000]">
       <nav className="flex items-center justify-between p-6">
         <Link href="/">
-          <h1 className="text-xl font-bold text-[#C5FF00] hover:scale-[101%]">
+          <h1 className="text-xl font-bold text-primary hover:scale-[101%]">
             FDM Flat Finder
           </h1>
         </Link>
@@ -24,7 +24,7 @@ const Header = () => {
               <ProfileButton user={user} />
             </>
           ) : (
-            <SignInButton className="rounded-full bg-[#C5FF00] px-4 py-2 hover:scale-[101%]" />
+            <SignInButton className="rounded-full bg-primary px-4 py-2 hover:scale-[101%]" />
           )}
         </div>
       </nav>
