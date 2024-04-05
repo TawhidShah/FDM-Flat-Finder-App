28b4216b723396188eac056eb783cbe46d94e69d
Tawhid Shah <tawhidshahdev@gmail.com>
2024-04-05 20:55:55 +0100

    Removed colored bg from fdm logo and removed erroneous font links

--- components/WelcomeMessage.jsx
+++ components/WelcomeMessage.jsx
@@ -1,15 +1,12 @@
 const WelcomeMessage = () => {
   return (
     <div className="mt-20 text-center">
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-      <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap" rel="stylesheet" />
       <img src="/images/FDMLogo.png" alt="FDM logo" className="mx-auto" />
 
-      <h1 className="mb-4 text-6xl font-bold text-white font-montserrat">
+      <h1 className="mb-4 text-6xl font-bold text-white">
         Welcome to FDM Flat Finder
       </h1>
-      <p className="mb-6 text-xl text-white font-montserrat">
+      <p className="font-montserrat mb-6 text-xl text-white">
         Find your perfect flat with ease.
       </p>
     </div>
