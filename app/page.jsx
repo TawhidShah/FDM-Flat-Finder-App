c218caad6d3eb8771e14c847594209020d9b28d2
Tawhid Shah <tawhidshahdev@gmail.com>
2024-04-07 03:14:51 +0100

    Moved welcome message inline, this fixed small logo problem

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,19 @@
 "use client";
 import Link from "next/link";
 
-import WelcomeMessage from "@/components/WelcomeMessage";
-
 export default function Home() {
   return (
     <main className="flex flex-1 flex-col items-center">
-      <WelcomeMessage />
+      <div className="mt-20 text-center">
+        <img src="/images/FDMLogo.png" alt="FDM logo" className="mx-auto" />
+
+        <h1 className="mb-4 text-6xl font-bold text-white">
+          Welcome to FDM Flat Finder
+        </h1>
+        <p className="font-montserrat mb-6 text-xl text-white">
+          Find your perfect flat with ease.
+        </p>
+      </div>
       <div className="mt-10">
         <Link
           href="/listings"
